6f7ad70f33222afeffd2f2753e0b7b06e055ac12
capic <vcapic@gmail.com>
2015-09-04 12:57:34 +0200

    init

--- scripts/install.py
+++ scripts/install.py
@@ -3,8 +3,34 @@ __author__ = 'Vincent'
 
 from lib.menu import menu
 import plow_solution
+import utils
+
+
+def chargement_config():
+    config = {}
+    execfile("../config.cfg", config)
+
+    if 'git_plow_back_rest' in config:
+        utils.GIT_PLOW_BACK_REST = config['git_plow_back_rest']
+    if 'repertoire_git_plow_back_rest' in config:
+        utils.REPERTOIRE_GIT_PLOW_BACK_REST = config['repertoire_git_plow_back_rest']
+    if 'repertoire_installation_plow_back_rest' in config:
+        utils.REPERTOIRE_INSTALLATION_PLOW_BACK_REST = config['repertoire_installation_plow_back_rest']
+    if 'mysql_login' in config:
+        utils.MYSQL_LOGIN = config['mysql_login']
+    if 'mysql_pass' in config:
+        utils.MYSQL_PASS = config['mysql_pass']
+    if 'mysql_host' in config:
+        utils.MYSQL_HOST = config['mysql_host']
+    if 'mysql_database' in config:
+        utils.MYSQL_DATABASE = config['mysql_database']
+    if 'notification_adresse' in config:
+        utils.NOTIFICATION_ADRESSE = config['notificaion_adresse']
+
 
 def main():
+    chargement_config()
+
     general_menu = menu.Menu()
     general_menu_selection = general_menu.show(
         {
